50355b04eac65e13c2f7f28ebc4f8fc1afb0847f
Aungthu772 <aungthuarun@gmail.com>
2018-02-02 19:54:02 +0630

    'error'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp'||'mongodb://arun:arun@ds221258.mlab.com:21258/data');
+mongoose.connect('mongodb://localhost:27017/TodoApp'||MONGOLAB_URI);
 
 module.exports={
   mongoose
