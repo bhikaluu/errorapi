0b42728eb7503650d120ee02ef64470021064e22
Aungthu772 <aungthuarun@gmail.com>
2018-02-02 20:33:09 +0630

    'error'

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const {User} = require('./models/user.js');
 
 var app = express();
 
-const port = process.argv.PORT||3000;
+const port = process.env.PORT||3000;
 app.use(bodyParse.json());
 app.post('/todos',(req,res)=>{
   var todo =new Todo({
