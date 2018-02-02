cf632f0911e7236f82e33c1514b40e4a804f6456
Aungthu772 <aungthuarun@gmail.com>
2018-02-02 20:53:17 +0630

    'master'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/data'||process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI||'mongodb://localhost:27017/data');
 
 module.exports={
   mongoose
